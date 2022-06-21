4bbb1ed0d878af1a95b166d5fe5f6731b5ed3e7a
Dom <Domalbano35@gmail.com>
2022-06-21 09:00:11 -0400

    working on night/day logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,66 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import { setContext } from "@apollo/client/link/context";
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Import the Apollo client wrapper
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
 
-// Import pages and components 
+// Import pages and components
 import SearchActivities from "./pages/SearchActivities";
 import ProfilePage from "./pages/ProfilePage";
 import ContactSupport from "./pages/ContactSupport";
 import Navbar from "./components/Navbar";
-// Changed import of "footer" to new component function name of "LabelBottomNavigation"
 import LabelBottomNavigation from "./components/footer.js";
 
 // TODO - Remove after refactor is complete
 import Home from "./components/homePage";
 
+import { useTheme, ThemeProvider, createTheme } from "@mui/material/styles";
+import lightTheme from "../src/components/assets/css/lightTheme";
+import darkTheme from "../src/components/assets/css/darkTheme";
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-    uri: "/graphql",
+  uri: "/graphql",
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
-    // get the authentication token from local storage if it exists
-    const token = localStorage.getItem("id_token");
-    // return the headers to the context so httpLink can read them
-    return {
-        headers: {
-            ...headers,
-            authorization: token ? `Bearer ${token}` : "",
-        },
-    };
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("id_token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
 });
 
 const client = new ApolloClient({
-    // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
 
 export default function App() {
-    return (
-      // Wrap everything in the ApolloProvider and client being passed in a `props`
-      <ApolloProvider client={client}>
-        <Router>
-          <>
-            <Navbar />
-            <Routes>
-              <Route path="/" element={<SearchActivities />} />
-              {/* <Route path="/profile" element={<ProfilePage />} /> */}
-              {/* <Route path="/support" element={<ContactSupport />} /> */}
-              {/* <Route path="*" element={<h1 className="display-2">Wrong page!</h1>} /> */}
+  const [theme, setTheme] = useState(darkTheme);
 
-              {/* <Route path="/old-home" element={<Home />} /> */}
-            </Routes>
-            {/* changed component name from footer to LabelBottomNavigation */}
-            <LabelBottomNavigation />
-          </>
-        </Router>
-      </ApolloProvider>
-    );
+  // let handleClick = () => {
+  //   theme === darkTheme ? setTheme(lightTheme) : setTheme(darkTheme);
+  // };
+
+  return (
+    // Wrap everything in the ApolloProvider and client being passed in a `props`
+    <ApolloProvider client={client}>
+      {/* <ThemeProvider theme={theme}> */}
+      <Router>
+        <>
+          <ThemeProvider theme={theme}>
+            <Navbar />
+          </ThemeProvider>
+          <Routes>
+            <Route path="/" element={<SearchActivities />} />
+            {/* <Route path="/profile" element={<ProfilePage />} /> */}
+            {/* <Route path="/support" element={<ContactSupport />} /> */}
+            {/* <Route path="*" element={<h1 className="display-2">Wrong page!</h1>} /> */}
+
+            {/* <Route path="/old-home" element={<Home />} /> */}
+          </Routes>
+          {/* changed component name from footer to LabelBottomNavigation */}
+          <LabelBottomNavigation />
+        </>
+      </Router>
+    </ApolloProvider>
+  );
 }
